22ed127131a9cffb292283f0db3359489acde47f
dagmawibabi <1babidagi@gmail.com>
2025-11-29 07:30:37 +0300

    init

--- datasets/enkokilish.ts
+++ datasets/enkokilish.ts
@@ -1,3 +1,8 @@
 export const enkokilish_dataset = [
   { input: "የሰጡትን የሚበላ ሲያጠጡት የሚጠፋ?", expected: "እሳት" },
+  { input: "ስሩ ወደ ላይ ጫፉ ወደታች?", expected: "የከብት ጅራት" },
+  { input: "ቀኑን ሙሉ በአየር ላይ የሚውል ሆኖም የትም የማይንቀሳቀስ?", expected: "ባንዲራ" },
+  { input: "ልብስም ጉርስም ትሆን?", expected: "በግ" },
+  { input: "ነጩ በሬ ጥቁሩን በሬ አስነስቶ ተኛበት?", expected: "ሽበት" },
+  { input: "አንድ ጊዜ ከወጡበት ሁለተኛ የማይመለሱበት?", expected: "የእናት መሃጸን" },
 ];
